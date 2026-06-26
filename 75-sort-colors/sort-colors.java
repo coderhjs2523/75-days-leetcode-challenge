b52ed63e9236ce34662281106a35239e2f607e97
Hareesh <hareeshjaiveersingh@gmail.com>
2026-06-26 20:55:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (76.04%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,27 +1,50 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        int n = nums.length, Count_0 = 0, Count_1 = 0, Count_2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0)
-                Count_0++;
-            else if (nums[i] == 1)
-                Count_1++;
-            else
-                Count_2++;
-        }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 
-        for (int i = 0; i < n; i++) {
-            if (Count_0 != 0) {
-                nums[i] = 0;
-                Count_0--;
-            } else if (Count_1 != 0) {
-                nums[i] = 1;
-                Count_1--;
-            } else {
-                nums[i] = 2;
-                Count_2--;
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int l = 0, r = n - 1, m = 0;
+        while (m <= r) {
+            if (nums[m] == 0) {
+                swap(nums, l, m);
+                l++;
+                m++;
+            }
+            else if (nums[m] == 2) {
+                swap(nums, r, m);
+                r--;
+            }
+            else {
+                m++;
             }
         }
+
+        // int n = nums.length, Count_0 = 0, Count_1 = 0, Count_2 = 0;
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] == 0)
+        //         Count_0++;
+        //     else if (nums[i] == 1)
+        //         Count_1++;
+        //     else
+        //         Count_2++;
+        // }
+
+        // for (int i = 0; i < n; i++) {
+        //     if (Count_0 != 0) {
+        //         nums[i] = 0;
+        //         Count_0--;
+        //     } else if (Count_1 != 0) {
+        //         nums[i] = 1;
+        //         Count_1--;
+        //     } else {
+        //         nums[i] = 2;
+        //         Count_2--;
+        //     }
+        // }
 
         // for(int i=0;i<n-1;i++){
         //     for(int j=0;j<n-1-i;j++){
