c678c9f385c2a2eb59fb8a15abbaae75576d415d
Hareesh <hareeshjaiveersingh@gmail.com>
2026-07-02 19:43:10 +0530

    Time: 574 ms (12.01%) | Memory: 42.8 MB (5.69%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -3,8 +3,8 @@ class Solution {
         int i = 1;
         int EvenNum = 2;
         int OddNum = 1;
-        int EvenSum = 0;
-        int OddSum = 0;
+        int EvenSum = 2;
+        int OddSum = 1;
         while (i < n) {
             EvenNum += 2;
             OddNum += 2;
@@ -12,10 +12,10 @@ class Solution {
             OddSum += OddNum;
             i++;
         }
-        int num = (EvenSum > OddSum) ? OddSum : EvenSum;
+        int num = Math.min(EvenSum, OddSum);
         for (int k = num; k >= 1; k--) {
             if (EvenSum % k == 0 && OddSum % k == 0)
-                return i;
+                return k;
         }
         return 1;
     }
