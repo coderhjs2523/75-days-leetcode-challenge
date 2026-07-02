99c3bafa9f9c12532618f65eea1ac9f7a46fe16c
Hareesh <hareeshjaiveersingh@gmail.com>
2026-07-02 19:48:24 +0530

    Time: 1 ms (83.33%) | Memory: 42.7 MB (38.87%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,22 +1,32 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int i = 1;
-        int EvenNum = 2;
-        int OddNum = 1;
-        int EvenSum = 2;
-        int OddSum = 1;
-        while (i < n) {
-            EvenNum += 2;
-            OddNum += 2;
-            EvenSum += EvenNum;
-            OddSum += OddNum;
-            i++;
-        }
-        int num = Math.min(EvenSum, OddSum);
-        for (int k = num; k >= 1; k--) {
-            if (EvenSum % k == 0 && OddSum % k == 0)
-                return k;
-        }
-        return 1;
+        // int EvenSum = n*(n+1)
+        // int OddSum = n*n
+        // gcd(n*n, n*(n+1))
+        // n*gcd(n,n+1) // to consecutive number gcd is always 1
+        // n*1
+
+        return n;
+
+
+
+        // int i = 1;
+        // int EvenNum = 2;
+        // int OddNum = 1;
+        // int EvenSum = 2;
+        // int OddSum = 1;
+        // while (i < n) {
+        //     EvenNum += 2;
+        //     OddNum += 2;
+        //     EvenSum += EvenNum;
+        //     OddSum += OddNum;
+        //     i++;
+        // }
+        // int num = Math.min(EvenSum, OddSum);
+        // for (int k = num; k >= 1; k--) {
+        //     if (EvenSum % k == 0 && OddSum % k == 0)
+        //         return k;
+        // }
+        // return 1;
     }
 }
