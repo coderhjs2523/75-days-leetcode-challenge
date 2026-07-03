85c0a7191b0801e5f6cf31449f54be5068a3821c
Hareesh <hareeshjaiveersingh@gmail.com>
2026-07-03 13:58:29 +0530

    Time: 1 ms (98.52%) | Memory: 47.7 MB (43.32%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -4,12 +4,20 @@ class Solution {
         int n = matrix[0].length;
 
         boolean firstCol = true;
+        boolean firstRow = true;
 
         for (int i = 0; i < m; i++) {
-
             if (matrix[i][0] == 0)
                 firstCol = false;
+        }
 
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0)
+                firstRow = false;
+
+        }
+
+        for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
                     matrix[i][0] = 0;
@@ -19,16 +27,19 @@ class Solution {
         }
 
         for (int i = m - 1; i >= 0; i--) {
-
             for (int j = n - 1; j >= 1; j--) {
 
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
-
             if (!firstCol) {
                 matrix[i][0] = 0;
+            }
+        }
+        if (!firstRow){
+            for(int j=0;j<n;j++){
+                matrix[0][j] = 0;
             }
         }
     }
