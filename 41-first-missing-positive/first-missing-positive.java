305b4b89fcb02ea0ee213a563e37d0ff233364fc
Hareesh <hareeshjaiveersingh@gmail.com>
2026-07-12 11:13:59 +0530

    Time: 1 ms (100.00%) | Memory: 71.3 MB (47.02%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,16 +1,44 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
-        boolean [] arr = new boolean[n+1];
-        for(int i =0;i<n;i++){
-            if(nums[i]>0 && nums[i]<=n){
-                int idx = nums[i];
-                arr[idx]=true;
-            }
+        boolean contain1 = false;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1)
+                contain1 = true;
+            if (nums[i] <= 0 || nums[i] > n)
+                nums[i] = 1;
         }
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]==false)return i;
+        if (contain1 == false)
+            return 1;
+
+        for (int i = 0; i < n; i++) {
+            int num = Math.abs(nums[i]);
+            int idx = num - 1;
+            if (nums[idx] < 0)
+                continue;
+            nums[idx] = nums[idx] * (-1);
         }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0)
+                return (i + 1);
+        }
+
         return n+1;
+        //Brute force Approach   
+
+        // int n = nums.length;
+        // boolean [] arr = new boolean[n+1];
+        // for(int i =0;i<n;i++){
+        //     if(nums[i]>0 && nums[i]<=n){
+        //         int idx = nums[i];
+        //         arr[idx]=true;
+        //     }
+        // }
+        // for(int i=1;i<arr.length;i++){
+        //     if(arr[i]==false)return i;
+        // }
+        // return n+1;
     }
 }
