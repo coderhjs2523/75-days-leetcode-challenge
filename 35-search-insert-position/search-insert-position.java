3b49fbe7ee3d63e580fe32f2f3e8385b22126c32
Hareesh <hareeshjaiveersingh@gmail.com>
2026-07-15 12:18:57 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (28.85%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -3,11 +3,10 @@ class Solution {
         int n = nums.length, start = 0, end = n - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            //if(nums[mid]==target)return mid;
-            if (nums[mid] >= target)
-                end = mid - 1;
-            else // nums[mid]<target
+            if (nums[mid] < target)
                 start = mid + 1;
+            else // nums[mid] >= target
+                end = mid - 1;
         }
         return start;
     }
