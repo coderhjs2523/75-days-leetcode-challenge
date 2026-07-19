aa14dd3fd97f3ede92cd9c8461e480c914381794
Hareesh <hareeshjaiveersingh@gmail.com>
2026-07-19 07:59:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (8.67%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,4 +1,17 @@
 class Solution {
+    public boolean findNum(int[][] matrix, int target,int rowIndex){
+        int colStart = 0, colEnd = matrix[0].length - 1;
+        while (colStart <= colEnd) {
+            int midCol = colStart + (colEnd - colStart) / 2;
+
+            if (matrix[rowIndex][midCol] < target) colStart = midCol + 1;
+            else if (matrix[rowIndex][midCol] > target) colEnd = midCol - 1;
+            else return true;
+
+            }
+        return false; 
+    }
+
     public boolean searchMatrix(int[][] matrix, int target) {
         int row = matrix.length, col = matrix[0].length;
 
@@ -7,29 +20,10 @@ class Solution {
         while (rowStart <= rowEnd) {
             int midRow = rowStart + (rowEnd - rowStart) / 2;
 
-            if (target >= matrix[midRow][0] && target <= matrix[midRow][col - 1]) {
-
-                int colStart = 0, colEnd = col - 1;
-
-                while (colStart <= colEnd) {
-                    int midCol = colStart + (colEnd - colStart) / 2;
-
-                    if (matrix[midRow][midCol] < target)
-                        colStart = midCol + 1;
-                    else if (matrix[midRow][midCol] > target)
-                        colEnd = midCol - 1;
-                    else
-                        return true;
-                }
-                return false; 
-            }
-
-            else if (matrix[midRow][0] < target)
-                rowStart = midRow + 1;
-            else
-                rowEnd = midRow - 1;
+            if (target >= matrix[midRow][0] && target <= matrix[midRow][col - 1]) return findNum(matrix, target, midRow);
+            else if (matrix[midRow][0] < target) rowStart = midRow + 1;
+            else rowEnd = midRow - 1;
         }
-
         return false;
     }
 }
