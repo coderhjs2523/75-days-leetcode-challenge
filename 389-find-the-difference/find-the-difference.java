49ba41427520c0f35455849ebcca846914c70ded
Hareesh <hareeshjaiveersingh@gmail.com>
2026-07-21 20:00:38 +0530

    Time: 3 ms (41.57%) | Memory: 43.2 MB (36.20%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        
+        for (int i = 0; i < str1.length; i++) {
+            if (str1[i] != str2[i]) {
+                return str2[i]; 
+            }
+        }
+        
+        return str2[str2.length - 1];
+    }
+}
