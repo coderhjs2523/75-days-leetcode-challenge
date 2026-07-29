58a8c5cd51a83d630c771d287d1ad20ff9a67b4b
Hareesh <hareeshjaiveersingh@gmail.com>
2026-07-29 08:49:16 +0530

    Time: 4 ms (87.80%) | Memory: 46.4 MB (89.13%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,26 +1,49 @@
+// class Solution {
+//     public String reverseWords(String s) {
+//         StringBuilder sb = new StringBuilder();
+//         String[] str = s.split(" ");
+//         for (int i = 0; i < str.length; i++) {
+
+//             char[] chArray = str[i].toCharArray();
+//             int start = 0;
+//             int end = chArray.length - 1;
+
+//             while (start < end) {
+//                 char temp = chArray[start];
+//                 chArray[start] = chArray[end];
+//                 chArray[end] = temp;
+//                 start++;
+//                 end--;
+//             }
+//             sb.append(new String(chArray));
+
+//             if (i != str.length - 1) {
+//                 sb.append(" ");
+//             }
+//         }
+//         return sb.toString();
+//     }
+// }
+
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder sb = new StringBuilder();
-        String[] str = s.split(" ");
-        for (int i = 0; i < str.length; i++) {
-            
-            char[] chArray = str[i].toCharArray();
-            int start = 0;
-            int end = chArray.length - 1;
+        char[] chArray = s.toCharArray();
+        int start = 0;
+        for (int i = 0; i <= chArray.length; i++) {
 
-            while (start < end) {
-                char temp = chArray[start];
-                chArray[start] = chArray[end];
-                chArray[end] = temp;
-                start++;
-                end--;
-            }
-            sb.append(new String(chArray));
+            if (i == chArray.length || chArray[i] == ' ') {
+                int end = i - 1;
 
-            if (i != str.length - 1) {
-                sb.append(" ");
+                while (start < end) {
+                    char temp = chArray[start];
+                    chArray[start] = chArray[end];
+                    chArray[end] = temp;
+                    start++;
+                    end--;
+                }
+                start = i + 1;
             }
         }
-        return sb.toString();
+        return new String(chArray);
     }
 }
